fcffb311217c6507cdb84b3bf865f4d685d02e9f
Shawki <shawki2207112@stud.kuet.ac.bd>
2026-05-31 16:18:01 +0600

    implemented the view-registrations button for admin

---
+++ view-registrations.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"
+CodeBehind="view-registrations.aspx.cs"
+Inherits="KBEC.view_registrations" %>
+
+<!DOCTYPE html>
+
+<html>
+<head runat="server">
+
+<title>View Registrations</title>
+
+<style>
+
+body{
+    background:#0b0f14;
+    color:white;
+    font-family:Arial;
+}
+
+.container{
+    width:95%;
+    margin:50px auto;
+}
+
+.grid{
+    width:100%;
+    border-collapse:collapse;
+}
+
+.grid th{
+    background:#f5c518;
+    color:black;
+    padding:12px;
+}
+
+.grid td{
+    padding:10px;
+    border-bottom:1px solid #333;
+}
+
+</style>
+
+</head>
+
+<body>
+
+<form runat="server">
+
+<div class="container">
+
+<h1>Event Registrations</h1>
+
+<asp:GridView
+ID="gvRegistrations"
+runat="server"
+AutoGenerateColumns="true"
+CssClass="grid" />
+
+</div>
+
+</form>
+
+</body>
+</html>
---
+++ view-registrations.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace KBEC
+{
+    public partial class view_registrations : System.Web.UI.Page
+    {
+        string cs =
+            ConfigurationManager
+            .ConnectionStrings["KBECConnection"]
+            .ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        void LoadData()
+        {
+            SqlConnection con =
+                new SqlConnection(cs);
+
+            string query = @"
+
+            SELECT
+
+            E.Title AS EventName,
+
+            U.FullName,
+
+            U.Roll,
+
+            U.Department,
+
+            U.Email
+
+            FROM Registrations R
+
+            INNER JOIN Users U
+            ON R.UserID = U.UserID
+
+            INNER JOIN Events E
+            ON R.EventID = E.EventID";
+
+            SqlDataAdapter da =
+                new SqlDataAdapter(query, con);
+
+            DataTable dt =
+                new DataTable();
+
+            da.Fill(dt);
+
+            gvRegistrations.DataSource = dt;
+
+            gvRegistrations.DataBind();
+        }
+    }
+}
